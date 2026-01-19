85d6e9692f8943d2d8cafda25a27503cf1f7fe52
unknown <asthasingh.singh2007@gmail.com>
2026-01-19 11:13:00 +0530

    added sum_for_loop

---
+++ sum_for_loop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class sum_for_loop {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter your preffered number");
+        int n= sc.nextInt();
+        int sum = 0;
+    for(int i=1;i<=n; i++){
+        sum=sum+i;
+    }
+    System.out.println("sum of numbers upto n "+sum);
+}
+}
