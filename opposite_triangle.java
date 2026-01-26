58fbde661a88916b067ec8bd010823c0233cd251
unknown <asthasingh.singh2007@gmail.com>
2026-01-26 23:45:33 +0530

    added opposite_triangle

---
+++ opposite_triangle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class opposite_triangle {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the number of rows:");
+        int a=sc.nextInt();
+       
+
+        for(int i=a; i>=1 ; i--){
+            for(int j=1 ; j<=i ; j++){
+              
+                System.out.print("*");
+            }
+              System.out.println();
+            }
+        }
+    }
