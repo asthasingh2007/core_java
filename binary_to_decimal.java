eb58ff3031b038e06b9e33f43d86df6d8073eb7e
unknown <asthasingh.singh2007@gmail.com>
2026-01-27 22:59:56 +0530

    added binary_to_decimal

---
+++ binary_to_decimal.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class binary_to_decimal {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the binary digits:");
+        int binary_num= sc.nextInt();
+
+        int ans =0; //converted decimal number.
+        int b=1; // 2^0 = 1 power of 2.
+        while(binary_num>0){
+            int unit_digit= binary_num %10;
+            ans += (unit_digit*b);
+            binary_num/=10;
+            b*=2;
+        }
+        System.out.println();
+    }
+}
