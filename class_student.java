3873cd8214329f6067ea8d050bd59f84d8b314b2
unknown <asthasingh.singh2007@gmail.com>
2026-01-28 23:44:42 +0530

    added class_student

---
+++ class_student.java
@@ -0,0 +1,11 @@
+public class class_student {
+    int id = 1197;
+    String name = "Astha";
+
+    public static void main(String[] args) {
+        class_student s = new class_student();
+        System.out.println(s.id);
+        System.out.println(s.name);
+}
+
+}
