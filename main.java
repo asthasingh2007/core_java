2eff785064aa880093d8389a1acf0c59ff190323
unknown <asthasingh.singh2007@gmail.com>
2026-01-29 21:29:06 +0530

    added student_detail

---
+++ main.java
@@ -0,0 +1,13 @@
+
+class student {
+    int roll_number;
+    String student_name;
+    }
+     public class main{
+        public static void main(String[] args) {
+            student obj1 = new student();
+            System.out.println(obj1.roll_number);
+            System.out.println(obj1.student_name);
+        }
+        
+}
