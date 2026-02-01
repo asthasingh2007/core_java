4e18afbda561649a1531d2934e550f26e116841e
unknown <asthasingh.singh2007@gmail.com>
2026-02-01 23:10:45 +0530

    added end_of_file

---
+++ end_of_file.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class end_of_file {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int i = 1;
+      while(sc.hasNext()){
+        System.out.println(i + " " + sc.nextLine());
+        i++;
+      }
+    }
+}
